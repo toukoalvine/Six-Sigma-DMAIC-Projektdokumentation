0e0f52d905ec6f2a6b904a6164a1cae0d0773427
Alvine <toukoalvine@yahoo.fr>
2025-07-09 21:29:43 +0200

    Update and rename streamlit_app.py to app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
-#import plotly.express as px
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
